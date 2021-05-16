039427d4fd387d0522c0892e9bf4194b6334cff1
Carolina Marques <carolinaresendemarques@ua.pt>
2021-05-16 19:15:04 +0100

    fixed script

--- test_gym.py
+++ test_gym.py
@@ -1,3 +1,4 @@
+# Import the gym module
 import gym
 
 # Create a breakout environment
@@ -9,7 +10,7 @@ env.render()
 
 is_done = False
 while not is_done:
-    # Perform a random action, returns the new frame, reward and whether the game is over
-    frame, reward, isdone,  = env.step(env.action_space.sample())
-    # Render
+  # Perform a random action, returns the new frame, reward and whether the game is over
+    frame, reward, is_done, _ = env.step(env.action_space.sample())
+  # Render
     env.render()
